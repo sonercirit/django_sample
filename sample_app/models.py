763af568512d2345ae03c659d7f04050a9d58b44
sonercirit <sonercirit@gmail.com>
2020-03-04 09:02:29 +0300

    adds models

--- sample_app/models.py
+++ sample_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Vehicle(models.Model):
+    plate = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.plate
+
+
+class NavigationRecord(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return self.vehicle.plate
