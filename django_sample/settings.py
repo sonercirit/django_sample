ce7708ce8d2d95b68ceccdb8c0dcc438af69b037
sonercirit <sonercirit@gmail.com>
2020-03-04 09:12:28 +0300

    adds SQL logging in debug mode

--- django_sample/settings.py
+++ django_sample/settings.py
@@ -117,3 +117,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(asctime)s-%(module)s-%(levelname)s :: %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s :: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': False,
+            'level': 'DEBUG'
+        },
+    }
+}
