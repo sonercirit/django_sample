4d8f975168ef49884e47793978bec7446eab15d5
sonercirit <sonercirit@gmail.com>
2020-03-04 09:08:35 +0300

    add models to admin config

--- sample_app/admin.py
+++ sample_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from sample_app.models import Vehicle, NavigationRecord
+
+admin.site.register(Vehicle)
+admin.site.register(NavigationRecord)
