043d8f4b0c26be12916e18404847fbbc89ba1b33
sonercirit <sonercirit@gmail.com>
2020-03-04 09:03:57 +0300

    adds view for last points

--- sample_app/views.py
+++ sample_app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.db.models import F
+from django.http import JsonResponse
 
 # Create your views here.
+from sample_app.models import NavigationRecord
+
+
+def last_points(request):
+    distinct = NavigationRecord.objects.order_by('vehicle', '-datetime').distinct('vehicle')
+    results = NavigationRecord.objects.filter(id__in=distinct).order_by('-datetime').annotate(
+        vehicle_plate=F('vehicle__plate')).values('latitude', 'longitude', 'vehicle_plate', 'datetime')
+    return JsonResponse(list(results), safe=False)
